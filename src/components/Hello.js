0522030b06e84c2ab27485ecba480f7d6e333d3d
JonathasCalderon <Jonathas.Calderon@fundacion-jala.org>
2020-08-14 21:23:59 -0400

    Proyecto creado

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "../App.module.css";
+
+const Hello = () => {
+    return (
+        <div className={styles.hello} >
+            Hello
+        </div>
+    )
+}
+
+export default Hello;
---
+++ src/components/World.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "../App.module.css"
+
+const World = () => {
+    return (
+        <div className={styles.world} >
+            World!
+        </div>
+    )
+}
+
+export default World;
